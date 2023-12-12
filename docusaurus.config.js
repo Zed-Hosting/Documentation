404c0f2f9852497230fec5b45008d46c39f016a3
Witt <adam.witt@iaawg.com>
2023-12-12 11:18:07 -0600

    updated footer
    Signed-off-by: Witt <adam.witt@iaawg.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -132,7 +132,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} Zed Hosting, Inc. Built with <3 for you!>.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Zed Hosting, Inc. Built with <3 by Us for You.`,
       },
       prism: {
         theme: prismThemes.github,
